3978fb8a8b088dfd695cba4566e0a4348d18b658
TechmageDE <techmage_de@gmx.de>
2017-04-15 14:18:50 +0200

    Build Workspace

---
+++ src/main/java/com/untt/icb/ImmersiveConveyorBelts.java
@@ -0,0 +1,10 @@
+package com.untt.icb;
+
+import com.untt.icb.reference.Reference;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME,  version = Reference.VERSION)
+public class ImmersiveConveyorBelts
+{
+
+}
---
+++ src/main/java/com/untt/icb/reference/Reference.java
@@ -0,0 +1,11 @@
+package com.untt.icb.reference;
+
+public class Reference
+{
+    public static final String MOD_ID = "icb";
+    public static final String MOD_NAME = "Immersive-Conveyor-Belts";
+    public static final String VERSION = "1.11.2-0.1";
+
+    public static final String CLIENT_PROXY_CLASS = "com.untt.icb.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "com.untt.icb.proxy.ServerProxy";
+}
