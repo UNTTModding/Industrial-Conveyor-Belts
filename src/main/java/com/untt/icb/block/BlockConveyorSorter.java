c112f80f775ce646900f3febbed8dfe464f24291
TechmageDE <techmage_de@gmx.de>
2017-04-18 19:26:18 +0200

    Sorter is now directional, "Added filter"

--- src/main/java/com/untt/icb/block/BlockConveyorSorter.java
+++ src/main/java/com/untt/icb/block/BlockConveyorSorter.java
@@ -1,25 +1,37 @@
 package com.untt.icb.block;
 
 import com.untt.icb.tileentity.TileEntityConveyorSorter;
+import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-public class BlockConveyorSorter extends BlockConveyorBase
+public class BlockConveyorSorter extends BlockConveyorBase implements ITileEntityProvider
 {
+    public static final PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
 
     private static final AxisAlignedBB BOUNDS = new AxisAlignedBB(0.1F, 0.0F, 0.1F, 0.9F, 1.0F, 0.9F);
 
     public BlockConveyorSorter(String name)
     {
         super(name);
+
+        this.setDefaultState(blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
     }
 
     @Override
@@ -43,6 +55,27 @@ public class BlockConveyorSorter extends BlockConveyorBase
     }
 
     @Override
+    public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack)
+    {
+        worldIn.setBlockState(pos, state.withProperty(FACING, placer.getHorizontalFacing()));
+
+        super.onBlockPlacedBy(worldIn, pos, state, placer, stack);
+    }
+
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        if (worldIn.getTileEntity(pos) != null && worldIn.getTileEntity(pos) instanceof TileEntityConveyorSorter)
+        {
+            TileEntityConveyorSorter tileSorter = (TileEntityConveyorSorter) worldIn.getTileEntity(pos);
+
+            tileSorter.addFilter(playerIn.getHeldItem(hand), facing);
+        }
+
+        return true;
+    }
+
+    @Override
     public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn)
     {
         if (!worldIn.isRemote)
@@ -50,7 +83,6 @@ public class BlockConveyorSorter extends BlockConveyorBase
             if (entityIn instanceof EntityItem)
             {
                 EntityItem entityItem = (EntityItem) entityIn;
-
 
                 if (worldIn.getTileEntity(pos) != null && worldIn.getTileEntity(pos) instanceof TileEntityConveyorSorter)
                 {
@@ -62,5 +94,26 @@ public class BlockConveyorSorter extends BlockConveyorBase
                 }
             }
         }
+    }
+
+    @Override
+    @Nonnull
+    @SuppressWarnings("deprecation")
+    public IBlockState getStateFromMeta(int meta)
+    {
+        return getDefaultState().withProperty(FACING, EnumFacing.getFront(meta + 2));
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state)
+    {
+        return (state.getValue(FACING)).getIndex() - 2;
+    }
+
+    @Override
+    @Nonnull
+    protected BlockStateContainer createBlockState()
+    {
+        return new BlockStateContainer(this, FACING);
     }
 }
--- src/main/java/com/untt/icb/tileentity/TileEntityConveyorSorter.java
+++ src/main/java/com/untt/icb/tileentity/TileEntityConveyorSorter.java
@@ -1,50 +1,70 @@
 package com.untt.icb.tileentity;
 
-import com.untt.icb.utility.LogHelper;
 import net.minecraft.entity.item.EntityItem;
-import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.Vec3d;
+
+import java.util.ArrayList;
 
 public class TileEntityConveyorSorter extends TileEntityConveyorBase
 {
+    private ArrayList<ItemStack> filterLeft = new ArrayList<>();
+    private ArrayList<ItemStack> filterRight = new ArrayList<>();
+    private ArrayList<ItemStack> filterCenter = new ArrayList<>();
+
     public TileEntityConveyorSorter()
     {
 
     }
 
+    public void addFilter(ItemStack stack, EnumFacing facing)
+    {
+        if (facing == getFacing().rotateYCCW())
+            filterLeft.add(stack);
+
+        else if (facing == getFacing().rotateY())
+            filterRight.add(stack);
+
+        else if (facing == getFacing().getOpposite())
+            filterCenter.add(stack);
+    }
+
     public void sortItemStack(ItemStack stack)
     {
-        LogHelper.info("Sort item: " + stack.getItem().getUnlocalizedName());
+        EnumFacing facingSorted = getSideForItem(stack);
 
-        float posX = pos.getX() + 0.5F;
-        float posY = pos.getY() + 0.2F;
-        float posZ = pos.getZ() + 0.5F;
+        Vec3d posSpawn = new Vec3d(pos.offset(facingSorted).getX() + 0.5D, pos.offset(facingSorted).getY() + 0.2D, pos.offset(facingSorted).getZ() + 0.5D);
+        Vec3d velocity = new Vec3d(0.1D * facingSorted.getFrontOffsetX(), 0.0D, 0.1D * facingSorted.getFrontOffsetZ());
 
-        float velX = 0.0F;
-        float velY = 0.0F;
-        float velZ = 0.0F;
-
-        if (stack.getItem().equals(Items.REDSTONE))
-        {
-            posX += 0.6F;
-            velX += 0.1F;
-        }
-
-        else if (stack.getItem().equals(Items.DIAMOND))
-        {
-            posZ -= 0.6F;
-            velZ -= 0.1F;
-        }
-
-        else
-        {
-            posZ += 0.6F;
-            velZ += 0.1F;
-        }
-
-        EntityItem entityItem = new EntityItem(world, posX, posY, posZ, stack);
-        entityItem.setVelocity(velX, velY, velZ);
+        EntityItem entityItem = new EntityItem(world, posSpawn.xCoord, posSpawn.yCoord, posSpawn.zCoord, stack);
+        entityItem.setVelocity(velocity.xCoord, velocity.yCoord, velocity.zCoord);
 
         world.spawnEntity(entityItem);
+    }
+
+    private EnumFacing getSideForItem(ItemStack stack)
+    {
+        EnumFacing facingSorter = getFacing();
+
+        if (filterContainsItem(stack, filterLeft))
+            return facingSorter.rotateYCCW();
+
+        else if (filterContainsItem(stack, filterRight))
+            return facingSorter.rotateY();
+
+        else if (filterContainsItem(stack, filterCenter))
+            return facingSorter;
+
+        else
+            return EnumFacing.UP;
+    }
+
+    private boolean filterContainsItem(ItemStack stack, ArrayList<ItemStack> filter)
+    {
+        for (ItemStack filterStack : filter)
+            if (stack.getItem() == filterStack.getItem()) return true;
+
+        return false;
     }
 }
